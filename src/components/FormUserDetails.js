bade6f978a50dfe928d213978cbd4b03076af486
Luis Castillo <castillovaliente@gmail.com>
2019-12-05 13:16:34 -0600

    first commit

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class FormUserDetails extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello from user Details</h1>
+            </div>
+        )
+    }
+}
+
+export default FormUserDetails
